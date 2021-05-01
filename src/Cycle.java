e0d1ce9f76a526502e3033bd1d9a28bdafd9caf9
ZXEFF <2958513754@qq.com>
2021-05-01 09:14:21 +0800

    1

--- src/Cycle.java
+++ src/Cycle.java
@@ -1,36 +1,29 @@
 public class Cycle {
+
     public void play() {
         System.out.println("Cycle");
     }
-    public void wheels(){
-        System.out.println("轮子的数量是"+"num");
-    }
+
 }
 class Unicycle extends Cycle {
     public void play() {
         System.out.println("Unicycle");
     }
-    public void wheels(){
-        System.out.println("Unicycle wheels:4");
-    }
+
 }
 
 class Bicycle extends Cycle {
     public void play() {
         System.out.println("Bicycle");
     }
-    public void wheels(){
-        System.out.println("Bicycle wheels:2");
-    }
+
 }
 
 class Tricycle extends Cycle {
     public void play() {
         System.out.println("Tricycle");
     }
-    public void wheels(){
-        System.out.println("Tricycle wheels:3");
-    }
+
 }
 class Ride {
     public static void ride(Cycle cycle) {
@@ -44,9 +37,7 @@ class Ride {
         ride(unicycle);
         ride(bicycle);
         ride(tricycle);
-        unicycle.wheels();
-        bicycle.wheels();
-        tricycle.wheels();
+
 
     }
 }
