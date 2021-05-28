2f92f6f699a05850be488d1b5614eb175f095a34
ZXEFF <2958513754@qq.com>
2021-05-28 22:17:24 +0800

    1

--- src/gui/FrameDemo.java
+++ src/gui/FrameDemo.java
@@ -1,16 +1,20 @@
+
 package gui;
-
+import gui.sin.GraphicPanel;
+import gui.sin.MyFrameSin;
 import javax.swing.*;
+import java.awt.*;
+import java.util.Calendar;
 
-    public class FrameDemo {
-        public static void main(String[] args) {
-            JFrame f2=new JFrame("hello");
-            MyFrame frame1=new MyFrame("第一个窗口");
-            frame1.setLocation(100,100);
-            MyFrameDate frame2=new MyFrameDate("第二个窗口");
-            frame2.showTime();
-
-
-        }
+public class FrameDemo {
+    public static void main(String[] args) {
+        //JFrame f2=new JFrame("hello");
+        //MyFrame frame1=new MyFrame("第一个窗口");
+        //frame1.setLocation(400,400);
+        //MyFrameDate frame2=new MyFrameDate("第二个窗口");
+        //frame2.showTime();
+        MyFrameSin frameSin=new MyFrameSin("正弦函数");
+        JSpinner graphicpanel=new JSpinner();
 
     }
+}
